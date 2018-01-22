4f191483eb6b45bf67c71276b9e5016e6c9eebfb
Tristan <bobtheunstroyer@gmail.com>
2018-01-21 18:24:25 -0700

    Added Clickinggit commit --amend

---
+++ RealGame/Assets/Scripts/MouseControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControls : MonoBehaviour {
+
+    public Camera mainCam;
+
+	void Start ()
+    {
+		
+	}
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            if (hit.collider.gameObject.tag == "Unit")
+            {
+                print(hit.collider.gameObject.name);
+            }
+        }
+	}
+}
